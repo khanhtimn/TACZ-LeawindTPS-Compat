13000266a247ccf192a46a9dd68b53d07a76b07f
KhanhTIMN <khanhlcbb@gmail.com>
2024-08-14 16:21:43 +0700

    First commit

---
+++ src/main/java/fun/teamti/tacztps/ThirdPersonTacz.java
@@ -0,0 +1,30 @@
+package fun.teamti.tacztps;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ThirdPersonTacz.MOD_ID)
+public class ThirdPersonTacz
+{
+    public static final String MOD_ID = "tacz_leawindtps_compat";
+
+    public ThirdPersonTacz()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+}
