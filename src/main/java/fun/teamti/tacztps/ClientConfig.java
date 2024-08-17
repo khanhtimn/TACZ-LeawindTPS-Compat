16b40f54eeff297b200715bec61463ebbb8e11a3
KhanhTIMN <khanhlcbb@gmail.com>
2024-08-18 04:02:51 +0700

    Add License

--- src/main/java/fun/teamti/tacztps/ClientConfig.java
+++ src/main/java/fun/teamti/tacztps/ClientConfig.java
@@ -15,12 +15,14 @@ public class ClientConfig {
                 .comment("""
                         If enabled, holding the aim button will put you in/out of first person view
                         This option overrides "switch_first_person_scoping"
-                        This only works while TACZ's Config: tacz-client.key.holdToAim is set to true""")
+                        This only works while TACZ's Config: tacz-client.key.holdToAim is set to true
+                        Default: false""")
                 .define("switch_first_person_aiming", false);
         SWITCH_FIRST_PERSON_SCOPING = BUILDER
                 .comment("""
                         If enabled, only when holding the aim button on a gun with a scope attachment will put you in/out of first person view
-                        This only works while TACZ's Config: tacz-client.key.holdToAim is set to true""")
+                        This only works while TACZ's Config: tacz-client.key.holdToAim is set to true
+                        Default: true""")
                 .define("switch_first_person_scoping", true);
         BUILDER.pop();
         SPEC = BUILDER.build();
